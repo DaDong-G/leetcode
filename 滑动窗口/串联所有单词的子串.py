77200431299de38e701baf089e7c130161e5e2d9
guojundong <1411789366@qq.com>
2022-03-23 14:19:49 +0800

    新增滑动窗口题目

--- 滑动窗口/串联所有单词的子串.py
+++ 滑动窗口/串联所有单词的子串.py
@@ -20,3 +20,87 @@
 # 来源：力扣（LeetCode）
 # 链接：https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+from collections import Counter
+
+
+# 方法一 ： 1. 使用两个hash ,{'word': 2, 'good': 1, 'best': 1}, {'word': 0, 'good': 0, 'best': 0}
+#          2. 每次移动1位的长度，然后在遍历words，然后判断两个hash的值是否一致。
+
+
+# 方法二： 优化方法一，每次移动 one_word 的长度。
+class Solution:
+    def findSubstring(self, s: str, words):
+        if not s or not words:
+            return []
+        word_fre = Counter(words)
+        one_word_len = len(words[0])
+        all_word_len = len(words) * one_word_len
+        str_len = len(s)
+        # i = 0
+        r = []
+        for f in range(one_word_len):
+            i = f
+            while i + all_word_len <= str_len:
+                current_word_free = Counter()
+                j, k = 0, one_word_len
+                for _ in words:
+                    # s[i + j : i + k] 是从i开始，一个单词的长度
+                    w = s[i + j: i + k]
+                    # print(w)
+                    if w in words:
+                        if current_word_free[w] > word_fre[w]:
+                            break
+                        else:
+                            current_word_free[w] += 1
+                    if w not in words:
+                        break
+                    # print(s[i + j: i + k])
+                    j += one_word_len
+                    k += one_word_len
+                if current_word_free == word_fre:
+                    r.append(i)
+                # print(i)
+                i += one_word_len
+        print(r)
+        return r
+
+# 方法三 使用滑动窗口，这个窗口是可变的，如果超出去了，左边的就增加。
+class Solution:
+    def findSubstring(self, s: str, words):
+        if not s or not words:
+            return []
+        word_fre = Counter(words)
+        one_word_len = len(words[0])
+        all_word_len = len(words)
+        str_len = len(s)
+        r = []
+        for f in range(one_word_len):
+            i, j = f, f
+            k = 0
+            current_word_free = Counter()
+            while i + one_word_len <= str_len:
+                w = s[j: j + one_word_len]
+                j += one_word_len
+                # 如果单词不在 words 列表里面，直接移到最右边
+                if w not in word_fre:
+                    i = j
+                    current_word_free.clear()
+                    k = 0
+                else:
+                    current_word_free[w] += 1
+                    k += 1
+                    # 如果单词超出，就移动最左边的单词，往右移动一个。
+                    while current_word_free[w] > word_fre[w]:
+                        left_word = s[i:i + one_word_len]
+                        current_word_free[left_word] -= 1
+                        k -= 1
+                        i += one_word_len
+                    if k == all_word_len:
+                        r.append(i)
+        return r
+
+
+st = "lingmindraboofooowingdingbarrwingmonkeypoundcake"
+words = ["fooo", "barr", "wing", "ding", "wing"]
+s = Solution()
+s.findSubstring(st, words)
